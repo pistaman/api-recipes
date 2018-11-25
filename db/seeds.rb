55d80ab5fc1649934fef5774faa6f411360ac3b6
nishi <pisces.nisikawa@gmail.com>
2018-11-26 02:20:21 +0900

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 # coding: utf-8
 
-Recipe.create(:title => 'チキンカレー', :making_time => '45分', :serves => '4人', :ingredients => '玉ねぎ,肉,スパイス', :cost => '1000', :created_at => '2016-01-10 12:10:12', :updated_at => '2016-01-10 12:10:12')
+Recipe.create(:id => 1, :title => 'チキンカレー', :making_time => '45分', :serves => '4人', :ingredients => '玉ねぎ,肉,スパイス', :cost => '1000', :created_at => '2016-01-10 12:10:12', :updated_at => '2016-01-10 12:10:12')
 
-Recipe.create(:title => 'オムライス', :making_time => '30分', :serves => '2人', :ingredients => '玉ねぎ,卵,スパイス,醤油', :cost => '700', :created_at => '2016-01-11 13:10:12', :updated_at => '2016-01-11 13:10:12')
+Recipe.create(:id => 2, :title => 'オムライス', :making_time => '30分', :serves => '2人', :ingredients => '玉ねぎ,卵,スパイス,醤油', :cost => '700', :created_at => '2016-01-11 13:10:12', :updated_at => '2016-01-11 13:10:12')
