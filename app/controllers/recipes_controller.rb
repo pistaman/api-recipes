0cf6ae1855d1c922192b05a2e20263a8208b10f7
nishi <pisces.nisikawa@gmail.com>
2018-11-26 02:27:27 +0900

    fix nest

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,7 +9,7 @@ class RecipesController < ApplicationController
 
   def show
     @recipe = Recipe.find(params[:id])
-    @recipe = { message: 'Recipe details by id', recipe: @recipe }
+    @recipe = { message: 'Recipe details by id', recipe: [@recipe] }
     render json: response_fields(@recipe)
   end
 
@@ -32,7 +32,7 @@ class RecipesController < ApplicationController
   def update
     @recipe = Recipe.find(params[:id])
     if @recipe.update(recipe_params)
-      render json: response_fields({ message: 'Recipe successfully updated!', recipe: @recipe })
+      render json: response_fields({ message: 'Recipe successfully updated!', recipe: [@recipe] })
     end
   end
 
